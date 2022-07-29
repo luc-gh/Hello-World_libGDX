71a12616cdf192969795e573a80ee946d175899a
Lucas2 <contatolucasoficial0@gmail.com>
2022-07-28 21:38:08 -0300

    Commit 2 — Desenho de um círculo

--- core/src/com/libgdx/hello/Hello_World_Game.java
+++ core/src/com/libgdx/hello/Hello_World_Game.java
@@ -5,47 +5,33 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+//Desenhos são feitos com OpenGL (Open Graphics Library)
+//Classe ShapeRenderer vai ser usadas para este jogo simples, mas existem renderizadores mais avançados em libGDX
+
 public class Hello_World_Game extends ApplicationAdapter {
-	SpriteBatch batch;  //variável de desenho gráfico
-	BitmapFont font;  //variável de fonte
+	ShapeRenderer shape;
 	
 	@Override
-	public void create () {  //chamada uma vez quando seu jogo é aberto pela primeira vez
-		batch = new SpriteBatch();
-		font = new BitmapFont();
+	public void create () {
+		shape = new ShapeRenderer();
 	}
 
-	/*
 	@Override
-	public void resize (int width, int height){}
-
-	chamada quando seu jogo é redimensionado, e uma vez depois create()é chamada com a largura e altura iniciais do jogo
-	*/
-
-	@Override
-	public void render () {  //chamada para atualizar o jogo, várias vezes p/ segundo, conforme o FPS de cada dispositivo
+	public void render () {
 		Gdx.gl.glClearColor(.25f,.25f,.25f,1); //Determina cor de fundo (usando classe Gdx)
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  //
-		batch.begin();
-		font.draw(batch,"Hello World!",Gdx.graphics.getWidth()/2f,Gdx.graphics.getHeight()/2f);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//Desenhando círculo na tela, com os parâmetros:
+		shape.begin(ShapeRenderer.ShapeType.Filled); //tipo
+		shape.setColor(0,1,0,1);         //cor (verde)
+		shape.circle(200,100,75);        //posição e raio
+		shape.end();
 	}
 
-	/*
-	* @Override
-	* public void pause(){}
-	* - chamada Android quando o aplicativo é enviado p/ segundo plano na área de trabalho, logo antes dispose() de ser chamada
-	*
-	* @Override
-	* public void resume(){}
-	* - é chamada apenas no Android, quando o aplicativo é trazido de volta ao primeiro plano
-	*/
-
-
 	@Override
-	public void dispose () {  //é chamada quando o jogo é encerrado
-		batch.dispose();
+	public void dispose () {
+		shape.dispose();
 	}
 }
